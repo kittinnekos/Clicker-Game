60ee77167966f22c6583c3ac065a496ab277bbbc
kittinnekos <131129020+kittinnekos@users.noreply.github.com>
2024-02-09 17:32:23 +0900

    [fix]プレハブにコンポーネントされているスクリプトを専用フォルダに移動

---
+++ Clicker Game/Assets/Scripts/Prefab/DragonClickListener.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using static GameData;
+using static GameData.TapStatus;
+
+public class DragonClickListener : MonoBehaviour
+{
+    void Start()
+    {
+        // Buttonコンポーネントがアタッチされているか確認
+        Button button = GetComponent<Button>();
+        if (button != null)
+        {
+            // ボタンがクリックされたときに呼び出されるメソッドを設定
+            button.onClick.AddListener(OnButtonClick);
+        }
+        else
+        {
+            Debug.LogError("Button component not found on the GameObject.");
+        }
+    }
+
+    void OnButtonClick()
+    {
+        isTap[(int)dragon] = true;
+    }
+}
