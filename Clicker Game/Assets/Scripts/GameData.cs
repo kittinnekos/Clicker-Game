0ea34e717b823e004b9731877a1a62c65ee208c5
kittinnekos <131129020+kittinnekos@users.noreply.github.com>
2024-02-09 17:46:13 +0900

    [fix]ゲームデータとタップした時の処理をするスクリプトを修正

--- Clicker Game/Assets/Scripts/GameData.cs
+++ Clicker Game/Assets/Scripts/GameData.cs
@@ -11,6 +11,30 @@ public static class GameData
         buyButtonCenter = 7,
         buyButtonDown = 8,
     }
+
+    private const int BASE_PRICE = 1;
+    
+    public const int MAX_PRICE_COUNT = 10;
+    public const int MAX_BUY_BUTTONS = 2;
+    public const int BASE_ADD_SCORE = 5;
+
     public static bool[] isTap = new bool[9];
-    public static int Score = 0;
+    public static int[,] BuyStack = new int[2,3];
+
+    public static int Score = 100;
+    public static int cullentBuyButtons = 0;
+
+    public static int [] price = new int [MAX_PRICE_COUNT]
+    {BASE_PRICE,
+     BASE_PRICE + 1,
+     BASE_PRICE + 2,
+     BASE_PRICE + 3,
+     BASE_PRICE + 4,
+     BASE_PRICE + 5,
+     BASE_PRICE + 6,
+     BASE_PRICE + 7,
+     BASE_PRICE + 8,
+     BASE_PRICE + 9,
+     };
+
 }
--- Clicker Game/Assets/Scripts/TapHandleManager.cs
+++ Clicker Game/Assets/Scripts/TapHandleManager.cs
@@ -8,6 +8,12 @@ using static GameData.TapStatus;
 
 public class TapHandleManager : MonoBehaviour
 {
+    BuyManager buyManager;
+    void Start()
+    {
+        buyManager = gameObject?.GetComponent<BuyManager>();
+    }
+
     void Update()
     {
         TapStatusHandler();
@@ -17,11 +23,13 @@ public class TapHandleManager : MonoBehaviour
     {
         switch(isTap)
         {
+            // スタートボタンを押したらインゲームにシーン遷移する
             case var istap when istap[(int)start] == true:
             SceneManager.LoadScene("InGame");
             isTap[(int)start] = false;
             break;
 
+            // ドラゴンかボスを押したらスコアを加算する
             case var istap when istap[(int)dragon] == true:
             Score++;
             isTap[(int)dragon] = false;
@@ -32,19 +40,30 @@ public class TapHandleManager : MonoBehaviour
             isTap[(int)boss] = false;
             break;
 
+            // 右矢印か左矢印を押すと購入ボタンを切り替える
             case var istap when istap[(int)selectButtonRight] == true:
+            cullentBuyButtons++;
+            isTap[(int)selectButtonRight] = false;
             break;
 
             case var istap when istap[(int)selectButtonLeft] == true:
+            cullentBuyButtons--;
+            isTap[(int)selectButtonLeft] = false;
             break;
 
             case var istap when istap[(int)buyButtonUp] == true:
+            BuyManager.TryPurchase((int)buyButtonUp-6); // 0を渡したいため-6している
+            isTap[(int)buyButtonUp] = false;
             break;
 
             case var istap when istap[(int)buyButtonCenter] == true:
+            BuyManager.TryPurchase((int)buyButtonCenter-6); // 1を渡したいため-6している
+            isTap[(int)buyButtonCenter] = false;
             break;
 
             case var istap when istap[(int)buyButtonDown] == true:
+            BuyManager.TryPurchase((int)buyButtonDown-6); // 2を渡したいため-6している
+            isTap[(int)buyButtonDown] = false;
             break;
 
             default:
