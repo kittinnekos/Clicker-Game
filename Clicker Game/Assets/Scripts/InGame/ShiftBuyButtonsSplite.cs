f0f26056d7a76a6e56a389c90fb26a4e6f8f38c5
kittinnekos <131129020+kittinnekos@users.noreply.github.com>
2024-02-09 17:39:42 +0900

    [add]現在の購入ボタンに合わせて画像が切り替わるようにするスクリプトを追加

---
+++ Clicker Game/Assets/Scripts/InGame/ShiftBuyButtonsSplite.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using static GameData;
+
+public class ShiftBuyButtonsSplite : MonoBehaviour
+{
+    public Sprite[] BuyButtonsUp = new Sprite[MAX_BUY_BUTTONS];
+    public Sprite[] BuyButtonsCenter = new Sprite[MAX_BUY_BUTTONS];
+    public Sprite[] BuyButtonsDown = new Sprite[MAX_BUY_BUTTONS];
+
+    private Image BuyButtonUpImg;
+    private Image BuyButtonSenterImg;
+    private Image BuyButtonDownImg;
+
+    void Start()
+    {
+        // シーン内のBuyButtonsの子オブジェクトからImageコンポーネントを取得する
+        Transform BuyButtonUpTF = GameObject.Find("BuyButtons").transform.Find("BuyButtonUp");
+        GameObject BuyButtonUpObj = BuyButtonUpTF.gameObject;
+
+        Transform BuyButtonCenterTF = GameObject.Find("BuyButtons").transform.Find("BuyButtonCenter");
+        GameObject BuyButtonCenterObj = BuyButtonCenterTF.gameObject;
+
+        Transform BuyButtonDownTF = GameObject.Find("BuyButtons").transform.Find("BuyButtonDown");
+        GameObject BuyButtonDownObj = BuyButtonDownTF.gameObject;
+
+        BuyButtonUpImg = BuyButtonUpObj.GetComponent<Image>();
+        BuyButtonSenterImg = BuyButtonCenterObj.GetComponent<Image>();
+        BuyButtonDownImg = BuyButtonDownObj.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        ShiftBuyButtons();
+    }
+
+    // 画像を今の購入ボタンに合わせて切り替える
+    void ShiftBuyButtons()
+    {
+        BuyButtonUpImg.sprite = BuyButtonsUp[cullentBuyButtons];
+        BuyButtonSenterImg.sprite = BuyButtonsCenter[cullentBuyButtons];
+        BuyButtonDownImg.sprite = BuyButtonsDown[cullentBuyButtons];
+    }
+}
